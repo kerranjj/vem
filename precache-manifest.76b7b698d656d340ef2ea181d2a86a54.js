ddbad011a29ed8b6e6c4837f277ba834272ce74c
kerranjj <kerranjbj@gmail.com>
2020-04-07 20:37:57 +0200

    npm build from master

---
+++ precache-manifest.76b7b698d656d340ef2ea181d2a86a54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c569287d30dfaf1ed8c018d0ca017edc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94b7f55a89ba3ae9519f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6210bf1ded25448980ff",
+    "url": "/static/js/2.2594f893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2594f893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b7f55a89ba3ae9519f",
+    "url": "/static/js/main.c0694e2f.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
